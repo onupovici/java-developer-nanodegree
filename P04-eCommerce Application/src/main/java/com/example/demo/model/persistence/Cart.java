16a4a208f5b75d3fe603be58d35a490dcd24c1d6
Anastasia Matienzo <astmatienzo@gmail.com>
2019-09-06 03:15:18 -0500

    Feature: Add Spring security and logging * Add authentication and authorization using JWT * Add SLF4J logging

--- P04-eCommerce Application/src/main/java/com/example/demo/model/persistence/Cart.java
+++ P04-eCommerce Application/src/main/java/com/example/demo/model/persistence/Cart.java
@@ -1,22 +1,11 @@
 package com.example.demo.model.persistence;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 @Table(name = "cart")
@@ -93,5 +82,15 @@ public class Cart {
 			total = new BigDecimal(0);
 		}
 		total = total.subtract(item.getPrice());
+	}
+
+	@Override
+	public String toString() {
+		return "Cart{" +
+				"id=" + id +
+				", items=" + items +
+				", user=" + user +
+				", total=" + total +
+				'}';
 	}
 }
